e3c5b68752a2c554ef86013309c330d706a0f711
rubenrtorrado <rubentorrado@gmail.com>
2019-09-18 18:00:10 -0400

    Enhancing Sokobaning Sokoban Library

--- Sokoban/Sokoban.py
+++ Sokoban/Sokoban.py
@@ -1,3 +1,5 @@
+from queue import PriorityQueue
+
 directions = [{"x":-1, "y":0}, {"x":1, "y":0}, {"x":0, "y":-1}, {"x":0, "y":1}]
 def getAllValidStates(state, maxStates=-1):
     states=[]
@@ -38,6 +40,7 @@ def nodeInsertion(sortedArray, newNode, balance):
     sortedArray.append(newNode)
 
 class Node:
+    balance = 0.5
     def __init__(self, state, parent, action):
         self.state = state
         self.parent = parent
@@ -50,7 +53,11 @@ class Node:
         children = []
         for d in directions:
             childState = self.state.clone()
-            childState.update(d["x"], d["y"])
+            crateMove = childState.update(d["x"], d["y"])
+            if childState.player["x"] == self.state.player["x"] and childState.player["y"] == self.state.player["y"]:
+                continue
+            if crateMove and childState.checkDeadlock():
+                continue
             children.append(Node(childState, self, d))
         return children
 
@@ -76,11 +83,14 @@ class Node:
 
     def __str__(self):
         return str(self.depth) + "," + str(self.state.getHeuristic()) + "\n" + str(self.state)
+    
+    def __lt__(self, other):
+        return self.getHeuristic()+Node.balance*self.getCost() < other.getHeuristic()+Node.balance*other.getCost()
 
 class Agent:
     def getSolution(self, state, maxIterations):
         return []
-
+    
 class BFSAgent(Agent):
     def getSolution(self, state, maxIterations=-1):
         iterations = 0
@@ -100,7 +110,7 @@ class BFSAgent(Agent):
                 visisted.add(current.getKey())
                 queue.extend(current.getChildren())
         return bestNode.getActions(), bestNode, iterations
-
+    
 class DFSAgent(Agent):
     def getSolution(self, state, maxIterations=-1):
         iterations = 0
@@ -144,10 +154,36 @@ class AStarAgent(Agent):
                     nodeInsertion(queue,c,balance)
                 # queue.extend(current.getChildren())
         return bestNode.getActions(), bestNode, iterations
+    
+class EnhancedAStarAgent(Agent):
+    def getSolution(self, state, balance=1, maxIterations=-1):
+        iterations = 0
+        bestNode = None
+        Node.balance = balance
+        queue = PriorityQueue()
+        queue.put(Node(state.clone(), None, None))
+        visisted = set()
+        while (iterations < maxIterations or maxIterations <= 0) and queue.qsize() > 0:
+            iterations += 1
+            # queue = sorted(queue, key=lambda node: balance*node.getCost() + node.getHeuristic())
+            current = queue.get()
+            if current.checkWin():
+                return current.getActions(), current, iterations
+            if current.getKey() not in visisted:
+                if bestNode == None or current.getHeuristic() < bestNode.getHeuristic():
+                    bestNode = current
+                elif current.getHeuristic() == bestNode.getHeuristic() and current.getCost() < bestNode.getCost():
+                    bestNode = current
+                visisted.add(current.getKey())
+                children = current.getChildren()
+                for c in children:
+                    queue.put(c)
+        return bestNode.getActions(), bestNode, iterations
 
 class State:
     def __init__(self):
         self.solid=[]
+        self.deadlocks = []
         self.targets=[]
         self.crates=[]
         self.player=None
@@ -207,17 +243,15 @@ class State:
                         self.crates.append({"x":x, "y":y})
                     if c=="." or c=="+" or c=="*":
                         self.targets.append({"x":x, "y":y})
+        self.intializeDeadlocks()
 
     def clone(self):
         clone=State()
         clone.width = self.width
         clone.height = self.height
-
-        for y in range(len(self.solid)):
-            clone.solid.append([])
-            for x in range(len(self.solid[y])):
-                clone.solid[y].append(self.solid[y][x])
-
+        # since the solid is not changing then copy by value
+        clone.solid = self.solid
+        clone.deadlocks = self.deadlocks
         clone.player={"x":self.player["x"], "y":self.player["y"]}
 
         for t in self.targets:
@@ -227,7 +261,58 @@ class State:
             clone.crates.append({"x":c["x"], "y":c["y"]})
 
         return clone
-
+    
+    def intializeDeadlocks(self):
+        sign = lambda x: int(x/max(1,abs(x)))
+        
+        self.deadlocks = []
+        for y in range(self.height):
+            self.deadlocks.append([])
+            for x in range(self.width):
+                self.deadlocks[y].append(False)
+                
+        corners = []
+        for y in range(self.height):
+            for x in range(self.width):
+                if x == 0 or y == 0 or x == self.width - 1 or y == self.height - 1 or self.solid[y][x]:
+                    continue
+                if (self.solid[y-1][x] and self.solid[y][x-1]) or (self.solid[y-1][x] and self.solid[y][x+1]) or (self.solid[y+1][x] and self.solid[y][x-1]) or (self.solid[y+1][x] and self.solid[y][x+1]):
+                    if not self.checkTargetLocation(x, y):
+                        corners.append({"x":x, "y":y})
+                        self.deadlocks[y][x] = True
+        
+        for c1 in corners:
+            for c2 in corners:
+                dx,dy = sign(c1["x"] - c2["x"]), sign(c1["y"] - c2["y"])
+                if (dx == 0 and dy == 0) or (dx != 0 and dy != 0):
+                    continue
+                walls = []
+                x,y=c2["x"],c2["y"]
+                if dx != 0:
+                    x += dx
+                    while x != c1["x"]:
+                        if self.checkTargetLocation(x,y) or self.solid[y][x] or (not self.solid[y-1][x] and not self.solid[y+1][x]):
+                            walls = []
+                            break
+                        walls.append({"x":x, "y":y})
+                        x += dx
+                if dy != 0:
+                    y += dy
+                    while y != c1["y"]:
+                        if self.checkTargetLocation(x,y) or self.solid[y][x] or (not self.solid[y][x-1] and not self.solid[y][x+1]):
+                            walls = []
+                            break
+                        walls.append({"x":x, "y":y})
+                        y += dy
+                for w in walls:
+                    self.deadlocks[w["y"]][w["x"]] = True
+    
+    def checkDeadlock(self):
+        for c in self.crates:
+            if self.deadlocks[c["y"]][c["x"]]:
+                return True
+        return False
+    
     def checkOutside(self, x, y):
         return x < 0 or y < 0 or x > len(self.solid[0]) - 1 or y > len(self.solid)
 
@@ -300,6 +385,8 @@ class State:
                     self.player["y"]=newY
                     crate["x"]=crateX
                     crate["y"]=crateY
+                    return True
+        return False
 
     def getKey(self):
         key=str(self.player["x"]) + "," + str(self.player["y"]) + "," + str(len(self.crates)) + "," + str(len(self.targets))
